0e84b54a43ae58d69423e59992d3851db285dd7f
Konrad Kucharski <133031514+GTKondur@users.noreply.github.com>
2026-06-13 17:58:37 +0200

    Update start.sh

--- ProjektFilmy/Filmy/docker/start.sh
+++ ProjektFilmy/Filmy/docker/start.sh
@@ -3,31 +3,27 @@ set -e
 
 cd /var/www/html
 
+mkdir -p /tmp/database
+touch /tmp/database/database.sqlite
+chmod 777 /tmp/database/database.sqlite
+chmod 777 /tmp/database
 
-chmod 777 /var/www/html/database 2>/dev/null  true
-chmod 777 /var/www/html/database/database.sqlite 2>/dev/null 
- true
 
-
-if [ ! -f .env ]; then
-    cp .env.example .env
-fi
-
+cp .env.example .env
 
 sed -i 's/APP_ENV=local/APP_ENV=production/' .env
 sed -i 's/APP_DEBUG=true/APP_DEBUG=false/' .env
 sed -i 's|APP_URL=http://localhost|APP_URL=https://filmy-app.salmonfield-6bcf4751.polandcentral.azurecontainerapps.io|' .env
+sed -i 's|DB_CONNECTION=sqlite|DB_CONNECTION=sqlite|' .env
 
+echo "DB_DATABASE=/tmp/database/database.sqlite" >> .env
 
 php artisan key:generate --force
 
-
 php artisan migrate --force
-
 
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf 
