b57fd08d800e87ddbc02e95e6e26561f93670012
Konrad Kucharski <133031514+GTKondur@users.noreply.github.com>
2026-06-13 17:34:17 +0200

    Update start.sh

--- ProjektFilmy/Filmy/docker/start.sh
+++ ProjektFilmy/Filmy/docker/start.sh
@@ -1,31 +1,33 @@
-bash#!/bin/sh
+#!/bin/sh
 set -e
 
 cd /var/www/html
 
-# Utwórz plik .env jeśli nie istnieje
+
+chmod 777 /var/www/html/database 2>/dev/null  true
+chmod 777 /var/www/html/database/database.sqlite 2>/dev/null 
+ true
+
+
 if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
-# Ustaw zmienne dla produkcji
+
 sed -i 's/APP_ENV=local/APP_ENV=production/' .env
 sed -i 's/APP_DEBUG=true/APP_DEBUG=false/' .env
 sed -i 's|APP_URL=http://localhost|APP_URL=https://filmy-app.salmonfield-6bcf4751.polandcentral.azurecontainerapps.io|' .env
 
-# Generuj klucz aplikacji
+
 php artisan key:generate --force
 
-# Uruchom migracje
-php artisan migrate --force
 
-sed -i 's|APP_URL=.*|APP_URL=https://filmy-app.salmonfield-6bcf4751.polandcentral.azurecontainerapps.io|' .env
-echo "ASSET_URL=https://filmy-app.salmonfield-6bcf4751.polandcentral.azurecontainerapps.io/" >> .env
+php artisan migrate --force
 
 
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Uruchom supervisor (nginx + php-fpm)
+
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
